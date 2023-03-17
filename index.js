e2b6dfa37487bbbc72a84603dad106489b0eb1ec
varunSinghal505 <127879860+varunSinghal505@users.noreply.github.com>
2023-03-17 21:44:34 +0530

    Password generator code added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*','?'];
 
+let subset = [];
+let a = 0;
+for (let i = 0; i < 11; i++) {
+    a = Math.floor(Math.random() * set.length);
+    subset.push(set[a]);
+}
+password = subset.join('');
+console.log(password);
